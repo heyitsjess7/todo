fa624e80f488687cdd7ba15a5936d71ad69df26d
heyitsjess7 <155516637+heyitsjess7@users.noreply.github.com>
2024-03-05 20:36:01 +0900

    first commit

---
+++ demo.js
@@ -0,0 +1,16 @@
+//data type
+//  when types in letters its called strings
+// numbers as in 1 10 5
+// boolean means true or false
+// null means nothing and write as null;
+// undefined written as undefined;
+
+// write words in quotes
+
+// variable can be compared like a wallet where we can store height = 180
+// let's say;
+height = 180;
+christ = true;
+
+console.log("height", height, christ)
+// meaning there are 3 elements inside the console
